22a99419feeffed47c9eca1070d8dfed7bc59d41
Giridharan S <122785014+GiridharanSanthoshkumar@users.noreply.github.com>
2024-01-31 16:25:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+let data=[];
+    
+
+    
+
+app.get("/",(req,res)=>{
+    console.log(data);
+    res.render("index.ejs",{cmts:data});
+});
+app.post("/adcmt",(req,res)=>{
+    const cmt={
+        comment:req.body.cmt,
+
+
+    }
+    data.push(cmt);
+    console.log(data);
+    console.log(req.body.cmt);
+    res.redirect("/");
+});
+app.post("/addpost",(req,res)=>{
+    let reply=req.body.rep;
+    let comm=req.body.cmt;
+    let findobj=data.find((d)=>{(d.commennt===comm)});
+    
+});
+app.listen("3000",()=>{
+    console.log("server started");
+})
